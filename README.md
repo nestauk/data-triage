26771d1881aac73aeeae8606043d74ab5474ddb0
Joel Klinger <joel.alexander.klinger@gmail.com>
2020-06-11 16:49:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Data Triage
+===========
+
+A lightweight framework for triaging new open data sources.
+
+Setup a new triage by doing (from your favourite UNIX shell):
+
+    bash start-triage.sh <DATASET NAME>
+
+
+What's the point?
+-----------------
+
+Introducing disparate open data sources into a single framework is a messy process,
+since a-priori you simply don't know the:
+
+1. Practicalities of the collection (collection time, cost, disk-space, codebase development time)
+2. Coverage of the data (geographic, temporal, ecosystem)
+3. Shape of the data (what "tables" and "columns" can you expect to get from the data?)
+
+
+I want to triage a new dataset!
+-------------------------------
+Great! Take a fork and send us over a tidy PR and we'd be happy to include your triage in our codebase.
+
---
+++ start-triage.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Check only one arg
+if (( $# != 1 ))
+then
+  echo -e "Usage:\n\n\tbash start-triage.sh <DATASET_NAME>\n\n(No spaces, obviously)"
+  exit 1
+fi
+
+# Check running from root directory
+if [[ "${PWD##*/}" != "data-triage" ]];
+then
+    echo -e "You must run this script from the `data-triage` directory."
+    exit 1
+fi
+
+NEW_DIR="datasets/${1}"
+
+# Check dir doesn't exist
+if [[ -d "${NEW_DIR}" ]]; then
+    echo -e "\nError:\n\t${NEW_DIR} already exists.\n"
+    exit 1
+fi
+
+
+# Check jupyter is installed
+if ! [ -x "$(command -v jupyter)" ]; then
+    echo -e "\nError:\n\tYou must install jupyter before continuing.\n"
+    exit 1
+fi
+
+cp -r datasets/.template ${NEW_DIR}
+sed -i -e "s/{{template}}/${1}/g" ${NEW_DIR}/*/*ipynb
+jupyter trust ${NEW_DIR}/*/*ipynb &> /dev/null
+rm ${NEW_DIR}/*/*ipynb-e
+
+echo -e "\nNew data triage directory created at\n\t${NEW_DIR}\n"
